e9fac221ba91b055eeef8272d15a3f808daaaa7f
pacbeckh <pacovanbeckhoven@gmail.com>
2014-07-30 08:39:13 +0200

    Use grunt options for task configuration
    See grunt-wiredep #92 for more information. This will be a breaking
    change for current configurations. To upgrade the configuration: wrap
    all options for the wiredep task in Gruntfile.js (except for the src
    option) in an options object.

--- tasks/wiredep.js
+++ tasks/wiredep.js
@@ -3,7 +3,7 @@
 function wiredep(grunt) {
   grunt.registerMultiTask('wiredep', 'Inject Bower components into your source code.', function () {
     this.requiresConfig(['wiredep', this.target, 'src']);
-    require('wiredep')(this.data);
+    require('wiredep')(this.options({src:this.data.src}));
   });
 }
 
