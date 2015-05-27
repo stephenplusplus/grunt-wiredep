e44156865491b88e0b58b2b22ed6228be55dafa9
Stephen Sawchuk <sawchuk@gmail.com>
2015-05-26 22:14:40 -0400

    Update wiredep.js

--- tasks/wiredep.js
+++ tasks/wiredep.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function wiredep(grunt) {
-  grunt.registerMultiTask('wiredep', 'Inject Bower components into your source code.', function () {
+  grunt.registerMultiTask('wiredep', 'Inject Bower packages into your source code.', function () {
     this.requiresConfig(['wiredep', this.target, 'src']);
-	// Extend the options object with the entire data object (instead of just .src) for backward compatibility.
-    require('wiredep')(this.options(this.data));
+
+    var options = this.options(this.data);
+    require('wiredep')(options);
   });
 }
 
